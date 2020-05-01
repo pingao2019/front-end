d66d7b02eb6cfcccaa5e3001a05ca2409e516b35
Chris Forrest <redrider_cr@yahoo.com>
2020-04-30 21:11:38 -0400

    nav links are up logo not displaying

--- med-cabinet/src/App.js
+++ med-cabinet/src/App.js
@@ -5,12 +5,39 @@ import SignInForm from './components/signInForm'
 import PrivateRoute from './components/PrivateRoute'
 import UserPage from './components/UserPage'
 import './App.css'
+import logo from './logo.svg'
 //
 import UserInputForm from './components/userInputForm'
+import styled from 'styled-components'
+
+const StyledNav = styled.div`
+      display:flex;
+      justify-content:space-between;
+      width:25%;
+      margin-left:65%;
+      margin-bottom:5%;
+`
 
 function App() {
   return (
     <div className="App">
+      <nav>
+            <StyledNav>
+                    <a href="index.html">Home</a>
+                    
+                    <a href="aboutUs.html">About</a>
+                   
+                    <a href="contact.html">Contact</a>
+                    
+                    <a href="strains.html">Strains</a>
+                    
+                    <a href="https://medicabinet.netlify.app/login">Login</a>
+                    
+                    <a href="https://medicabinet.netlify.app/signup">Sign-Up</a>
+                
+            </StyledNav>
+            <img src="./logo.svg" alt="logo" className = "nav-logo"/>
+        </nav>
       <Switch>
         <PrivateRoute exact path={'/userpage'} component={UserPage}/>
         <PrivateRoute exact path={'/addtreatment'} component={UserInputForm} />
